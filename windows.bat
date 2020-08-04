915c162f381852d83d0d3a67d497646f15e5eb59
Chehui Chou <deadshot465@users.noreply.github.com>
2020-08-04 21:22:30 +0900

    Attempt to write Windows batch file again.

--- windows.bat
+++ windows.bat
@@ -1,5 +1,5 @@
 py -3 -m venv .
-./Scripts/activate
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& './Scripts/activate'"
 python -m pip install --upgrade pip
 pip install discord.py[voice]
 pip install python-dotenv
