b77267f6ee8d5494166a10fd1abd2fe049538c25
deja <26201406+iamdeja@users.noreply.github.com>
2021-04-15 20:21:29 +0300

    Make conversion function's input checking more robust

--- cogs/utility.py
+++ cogs/utility.py
@@ -1,27 +1,31 @@
 from discord.ext import commands
 from utils.utils import search_user
 from utils.convert_dictionary import get_convert_dictionary
-import json
 import re
 import typing
-
 
 EMOTE_REGEX = r'(<a?:\w+:\d+>)'
 EMOTE_ID_REGEX = r'[^:]+(?=>)'
 EMOTE_IS_ANIMATED_REGEX = r'(<a)'
 EMOTE_BASE_LINK = 'https://cdn.discordapp.com/emojis/'
 
-# Account for Aliases durring conversion
+
+# Account for Aliases during conversion
 def parse_alias(unit):
     if unit == "lb":
         return "lbs"
     else:
         return unit
 
+
+# Return metric relationship to its base unit (g, m, ...), or 1 if
+# not metric, or input is already a metric base unit
+# Do not change 1 to False, as the return value gets used for later arithmetic operations
 def check_metric(input):
     metric_regx = "([umk][g]|[mcdh][l]|[umcdk][m])"
     metric_match = re.match(metric_regx, input)
-    if metric_match == None: return 1
+    if metric_match is None:
+        return 1
 
     multiplier = {
         "u": 1e-6,
@@ -33,8 +37,9 @@ def check_metric(input):
     }
     return multiplier[input[-2]]
 
+
 def cvt_units(unit1: str, unit2: str, unit2_metr: str, value: float):
-    # The dictionary is stored in a seprate script
+    # The dictionary is stored in a separate script
     convert_dictionary = get_convert_dictionary()
 
     output = 0.0
@@ -54,7 +59,7 @@ def cvt_units(unit1: str, unit2: str, unit2_metr: str, value: float):
             output = value + 273.15
             unit_target = 'K'
         elif unit2 == 'f':
-            output = value * 9/5 + 32
+            output = value * 9 / 5 + 32
             unit_target = '\u2109'
         else:
             error = True
@@ -68,7 +73,7 @@ def cvt_units(unit1: str, unit2: str, unit2_metr: str, value: float):
             if unit2 == 'c':
                 unit_target = '\u2103'
             elif unit2 == 'k':
-                output = (value - 32) * 5/9 + 273.15
+                output = (value - 32) * 5 / 9 + 273.15
                 unit_target = 'K'
             else:
                 error = True
@@ -82,11 +87,11 @@ def cvt_units(unit1: str, unit2: str, unit2_metr: str, value: float):
             if unit2 == 'c':
                 unit_target = '\u2103'
             elif unit2 == 'f':
-                output = (value - 273.15) * 9/5 + 32
+                output = (value - 273.15) * 9 / 5 + 32
                 unit_target = '\u2109'
             else:
                 error = True
-    
+
     # If we don't need special calculations, use the Dictionary
     else:
         if unit1 in convert_dictionary and unit2 in convert_dictionary[unit1]:
@@ -97,7 +102,7 @@ def cvt_units(unit1: str, unit2: str, unit2_metr: str, value: float):
     if error:
         return 'This conversion is not possible!'
     else:
-        return [value, output, unit_source , unit2_metr + unit_target ]
+        return [value, output, unit_source, unit2_metr + unit_target]
 
 
 class Utility(commands.Cog):
@@ -156,41 +161,68 @@ class Utility(commands.Cog):
         await ctx.send(member[0].avatar_url)
 
     @commands.command(description='Convert units.',
-                      help='This command will help you convert between units.',
+                      help='This command will help you convert between units. Ex: `cvt m 1ft`.',
                       aliases=['convert'])
-    async def cvt(self, ctx: commands.Context, unit_res: str, input: str):
-        input_regx = "(-?[\\d.]+)(\\D{1,3})"
-        input_match = re.match(input_regx, input)
+    async def cvt(self, ctx: commands.Context, target_unit: str = '', input: str = ''):
+        # Ensure parameters exist
+        if not target_unit:
+            await ctx.send("Please provide a target unit.")
+            return
+        if not input:
+            await ctx.send("Please provide a source value.")
+            return
 
-        # Check if input is in the correct and expected format
-        if input_match == None:
+        # Ensure target unit is known
+        # A more complex implementation will be needed if conflicting types
+        # such as seconds are added (s & lbs)
+        convert_dictionary = get_convert_dictionary()
+        if target_unit not in convert_dictionary and target_unit[-1] not in convert_dictionary:
+            await ctx.send("Target unit of unknown type.")
+            return
+
+        # Regular expressions for parsing the unit and value from the input value
+        unit_regx = "\\D{1,3}"
+        value_regx = "-?[\\d.]+"
+
+        # Check if the source value is numerical and suffixed by a unit
+        input_match = re.match("(%s)(%s)" % (value_regx, unit_regx), input)
+
+        if input_match is None:
             await ctx.send("I don't understand what you mean by {}.".format(input))
             return
         # In theory, this should never trigger
         if input_match.string != input:
-            await ctx.send("I don't understand what you mean by {}.".format(input))
+            await ctx.send(
+                "I don't understand what you mean by {}.".format(input) +
+                " (Type assertion failed, please tell the developers.)")
             return
-            
-        # Get the value as numeric digits
-        value = int(re.findall("-?[\\d.]+", input)[0])
+
+        # Get the source unit and value
+        source_unit = re.findall(unit_regx, input)[0]
+        source_value = int(re.findall(value_regx, input)[0])
+
+        # Ensure source unit is known
+        # Source and target units could be checked together for a more 'elegant' solution
+        # A more complex implementation will be needed if conflicting types
+        # such as seconds are added (s & lbs)
+        if source_unit not in convert_dictionary and source_unit[-1] not in convert_dictionary:
+            await ctx.send("Source unit of unknown type.")
+            return
 
         # Check and apply logic if we are using the metric system as an input
-        unit1_metr = ""
-        unit1 = re.findall("\\D{1,3}", input)[0]
-        metric_multiplier_inp = check_metric(unit1)
+        metric_multiplier_inp = check_metric(source_unit)
         if metric_multiplier_inp != 1:
-            unit1 = input[-1]
-            unit1_metr = input[-2]
-            value *= metric_multiplier_inp
+            source_unit = input[-1]
+            source_value *= metric_multiplier_inp
 
-        unit2_metr = ""
-        unit2 = unit_res
-        metric_multiplier_res = check_metric(unit_res) 
+        # Get appropriate target unit, if of the metric system
+        target_unit_metric = ""
+        metric_multiplier_res = check_metric(target_unit)
         if metric_multiplier_res != 1:
-            unit2 = unit_res[-1]
-            unit2_metr = unit_res[-2]
+            target_unit_metric = target_unit[-2]
+            target_unit = target_unit[-1]
 
-        answer = cvt_units(parse_alias(unit1), parse_alias(unit2), unit2_metr, value)
+        answer = cvt_units(parse_alias(source_unit), parse_alias(target_unit), target_unit_metric, source_value)
         if isinstance(answer, str):
             await ctx.send(answer)
         elif isinstance(answer, list):
