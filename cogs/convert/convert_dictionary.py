f7d1db22332fd3c99f03ba32480d29cb89473247
zsotroav <49715074+zsotroav@users.noreply.github.com>
2021-04-16 22:03:38 +0200

    Fix/convert (#14)
    * Fix conversion from Fahrenheit to Kelvin.
    
    * Fix conversion from Kelvin to Fahrenheit
    
    * Update conversion logic.
    
    * convert.json is no longer needed
    
    * improvements
    
    * Reworked args and Metric system WIP
    
    * Fixed most bugs, going to clean up the code
    
    * Make conversion function's input checking more robust
    
    * bug fixes
    
    * bug fixes and cleanup
    
    * move code to separate file
    
    Co-authored-by: deja <26201406+iamdeja@users.noreply.github.com>

---
+++ cogs/convert/convert_dictionary.py
@@ -0,0 +1,54 @@
+def get_convert_dictionary():
+    convert_dictionary = {
+        # Lenght
+        "m": {
+            "m": 1,
+            "in": 39.37,
+            "ft": 3.28084,
+            "mi": 6.2137e-4,
+            "au": 6.6846e-12
+        },
+        "in": {
+            "m": 0.0254,
+            "in": 1,
+            "ft": 0.0833,
+            "mi": 1.5783e-5,
+            "au": 1.6979e-13
+        },
+        "ft": {
+            "m": 0.3048,
+            "in": 12,
+            "ft": 1,
+            "mi": 1.8939e-3,
+            "au": 2.0375e-12
+        },
+        "mi": {
+            "m": 1609.34,
+            "in": 63360,
+            "ft": 5280,
+            "mi": 1,
+            "au": 2.0375e-12
+        },
+        "au": {
+            "m": 1.496e11,
+            "in": 5.89e12,
+            "ft": 4.908e+11,
+            "mi": 9.296e+7,
+            "au": 1
+        },
+        # Weight
+        "g": {
+            "g": 1,
+            "lbs": 0.00220462
+        },
+        "lbs": {
+            "g": 453.592,
+            "lbs": 1
+        },
+        # Temperatures for compatibility reasons
+        "c": {}, 
+        "f": {},
+        "k": {}
+    }
+    
+    return convert_dictionary
